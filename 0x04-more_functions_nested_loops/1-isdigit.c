598c3de3b2bac02a0c5f275fdc757cae59496a0e
Igbemo <aymayorr@gmail.com>
2023-02-23 15:40:48 -0800

    my multiplicstion

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * main - checks if input is a character or digit
+ *@c: functons check conditions
  * returns 1 if characters and zero if number
  */
-
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - multiplies the numbers
+ * functions - mulcitplies in accoirdance 
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
