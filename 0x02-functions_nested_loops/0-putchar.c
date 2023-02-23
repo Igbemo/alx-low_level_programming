8f183677ab121715fa44e71513c800c9ee1edada
Igbemo <aymayorr@gmail.com>
2023-02-22 16:30:12 -0800

    the pustchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints char
+ * main - prints putchar
+ * description - prints characters
  * retun 0 if successful
+ * @void - void function type
  */
 
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
