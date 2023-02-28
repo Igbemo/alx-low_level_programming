fd8a82eda8c2f84b41dea2a337b3746128af42a0
Igbemo <aymayorr@gmail.com>
2023-02-27 16:40:21 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,12 @@
-#include 'main.h'
-/**
-  main -swap valuses
-  a -is the firas tvalue
-  b - is the second value
-  */
+#include "main.h"
+/*
+   main- swap values
+   a- is first value
+   b- is second value
+   */
 
 void swap_int(int *a, int *b)
 {
 	int i = *a;
-	*a = *b;
-	*b = i;
-}
+	*a = *b
+	*b = 
