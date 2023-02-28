fcab8db6f927c56dfc3e4be8382c1624adfdc21f
Igbemo <aymayorr@gmail.com>
2023-02-28 14:43:59 -0800

    cool length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _strlen - returns the length of the string in c programming
  * @s: return lenght of string
  * Description: returns the length of a given string
  * Return: length of string
  **/
-
-size_t _strlen(const char *str)
+size_t _strlen(char *str)
 {
 	size_t length = 0;
 
-	while (**str++)
-	length++
+	while (*str++)
+	length++;
 	return (length);
 }
